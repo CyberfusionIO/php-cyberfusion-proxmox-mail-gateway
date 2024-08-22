e0b1e42c7feffcd26020a340ccd221af494e1398
Lars Jansen <lars@ljpc.nl>
2024-08-22 14:04:00 +0200

    feat: /statistics/domains endpoint

---
+++ src/Endpoints/Statistics/DomainsEndpoint.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Endpoints\Statistics;
+
+use Cyberfusion\ProxmoxMGW\Endpoints\Endpoint;
+use Cyberfusion\ProxmoxMGW\Models\DomainStatistics;
+use Cyberfusion\ProxmoxMGW\Requests\DomainsRequest;
+use Cyberfusion\ProxmoxMGW\Support\Result;
+use Illuminate\Support\Arr;
+use Throwable;
+
+/**
+ * Endpoint for fetching mail domains statistics.
+ */
+class DomainsEndpoint extends Endpoint {
+		/**
+		 * Get mail domains statistics.
+		 *
+		 * @param DomainsRequest $request
+		 *
+		 * @return Result
+		 */
+		public function get( DomainsRequest $request ): Result {
+				try {
+						$response = $this->client->makeRequest(
+								endpoint: '/statistics/domains',
+								method  : 'GET',
+								params  : [
+										'day'       => $request->day,
+										'endtime'   => $request->endtime,
+										'month'     => $request->month,
+										'starttime' => $request->starttime,
+										'year'      => $request->year,
+								]
+						);
+
+						$data = json_decode( $response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR );
+				} catch ( Throwable $exception ) {
+						return new Result(
+								success: false,
+								message: $exception->getMessage()
+						);
+				}
+
+				$statistics = collect();
+				foreach ( Arr::get( $data, 'data', [] ) as $stat ) {
+						$statistics->push( new DomainStatistics(
+								bytes_in      : Arr::get( $stat, 'bytes_in', 0 ),
+								bytes_out     : Arr::get( $stat, 'bytes_out', 0 ),
+								count_in      : Arr::get( $stat, 'count_in', 0 ),
+								count_out     : Arr::get( $stat, 'count_out', 0 ),
+								domain        : Arr::get( $stat, 'domain', '' ),
+								spamcount_in  : Arr::get( $stat, 'spamcount_in', 0 ),
+								spamcount_out : Arr::get( $stat, 'spamcount_out', 0 ),
+								viruscount_in : Arr::get( $stat, 'viruscount_in', 0 ),
+								viruscount_out: Arr::get( $stat, 'viruscount_out', 0 )
+						) );
+				}
+
+				return new Result(
+						success: true,
+						data   : [
+								'statistics' => $statistics,
+						]
+				);
+		}
+}
---
+++ src/Requests/DomainsRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Requests;
+
+/**
+ * Request class for fetching mail domains statistics.
+ */
+class DomainsRequest
+{
+		/**
+		 * @param int|null $day Day of month. Get statistics for a single day. (1-31)
+		 * @param int|null $endtime Only consider entries older than 'endtime' (unix epoch). This is set to '<start> + 1day' by default.
+		 * @param int|null $month Month. You will get statistics for the whole month if you do not specify a day. (1-12)
+		 * @param int|null $starttime Only consider entries newer than 'starttime' (unix epoch). Default is 'now - 1day'.
+		 * @param int|null $year Year. Defaults to current year. You will get statistics for the whole year if you do not specify a month or day. (1900-3000)
+		 */
+		public function __construct(
+				public ?int $day = null,
+				public ?int $endtime = null,
+				public ?int $month = null,
+				public ?int $starttime = null,
+				public ?int $year = null
+		) {}
+}
