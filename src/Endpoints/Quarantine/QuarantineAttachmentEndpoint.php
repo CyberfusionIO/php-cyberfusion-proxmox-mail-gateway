eb7fe5320b61d5bf83e522ec0f5733273bd9b217
Lars Jansen <lars@ljpc.nl>
2024-08-22 14:53:09 +0200

    feat: /quarantine/attachment endpoint

---
+++ src/Endpoints/Quarantine/QuarantineAttachmentEndpoint.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Endpoints\Quarantine;
+
+use Cyberfusion\ProxmoxMGW\Endpoints\Endpoint;
+use Cyberfusion\ProxmoxMGW\Models\QuarantineAttachment;
+use Cyberfusion\ProxmoxMGW\Requests\QuarantineAttachmentGetRequest;
+use Cyberfusion\ProxmoxMGW\Support\Result;
+use Illuminate\Support\Arr;
+use Throwable;
+
+/**
+ * Class QuarantineAttachmentEndpoint
+ *
+ * This class handles operations related to quarantined attachment mails in the Proxmox Mail Gateway.
+ */
+class QuarantineAttachmentEndpoint extends Endpoint
+{
+    /**
+     * Get a list of quarantined attachment mails in the given timeframe.
+     *
+     * @param QuarantineAttachmentGetRequest $request The request object containing query parameters.
+     * @return Result The result object containing the list of quarantined attachment mails or error information.
+     */
+    public function get(QuarantineAttachmentGetRequest $request): Result
+    {
+        try {
+            $response = $this->client->makeRequest(
+                endpoint: '/quarantine/attachment',
+                method: 'GET',
+                params: $request->toArray()
+            );
+
+            $data = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (Throwable $exception) {
+            return new Result(
+                success: false,
+                message: $exception->getMessage()
+            );
+        }
+
+        $quarantineAttachments = collect();
+        foreach ($data as $item) {
+            $quarantineAttachments->push(new QuarantineAttachment(
+                bytes: Arr::get($item, 'bytes'),
+                envelope_sender: Arr::get($item, 'envelope_sender'),
+                from: Arr::get($item, 'from'),
+                id: Arr::get($item, 'id'),
+                receiver: Arr::get($item, 'receiver'),
+                sender: Arr::get($item, 'sender'),
+                subject: Arr::get($item, 'subject'),
+                time: Arr::get($item, 'time')
+            ));
+        }
+
+        return new Result(
+            success: true,
+            data: [
+                'quarantineAttachments' => $quarantineAttachments
+            ]
+        );
+    }
+}
---
+++ src/Models/QuarantineAttachment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Models;
+
+/**
+ * Class QuarantineAttachment
+ *
+ * This class represents a quarantined attachment mail entry from the Proxmox Mail Gateway.
+ */
+class QuarantineAttachment
+{
+    /**
+     * @param int $bytes Size of raw email.
+     * @param string $envelope_sender SMTP envelope sender.
+     * @param string $from Header 'From' field.
+     * @param string $id Unique ID
+     * @param string $receiver Receiver email address
+     * @param string|null $sender Header 'Sender' field.
+     * @param string $subject Header 'Subject' field.
+     * @param int $time Receive time stamp
+     */
+    public function __construct(
+        public int $bytes,
+        public string $envelope_sender,
+        public string $from,
+        public string $id,
+        public string $receiver,
+        public ?string $sender,
+        public string $subject,
+        public int $time
+    ) {
+    }
+}
