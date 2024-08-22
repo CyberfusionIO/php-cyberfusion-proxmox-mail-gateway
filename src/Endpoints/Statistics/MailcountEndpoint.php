5434e0f9c6c47a8d8c9fa4a93923b82d6e6638bd
Lars Jansen <lars@ljpc.nl>
2024-08-22 14:07:40 +0200

    feat: /statistics/mailcount endpoint

---
+++ src/Endpoints/Statistics/MailcountEndpoint.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Endpoints\Statistics;
+
+use Cyberfusion\ProxmoxMGW\Endpoints\Endpoint;
+use Cyberfusion\ProxmoxMGW\Models\MailcountStatistics;
+use Cyberfusion\ProxmoxMGW\Requests\MailcountRequest;
+use Cyberfusion\ProxmoxMGW\Support\Result;
+use Illuminate\Support\Arr;
+use Throwable;
+
+/**
+ * Endpoint for fetching mail count statistics.
+ */
+class MailcountEndpoint extends Endpoint {
+		/**
+		 * Get mail count statistics.
+		 *
+		 * @param MailcountRequest $request
+		 *
+		 * @return Result
+		 */
+		public function get( MailcountRequest $request ): Result {
+				try {
+						$response = $this->client->makeRequest(
+								endpoint: '/statistics/mailcount',
+								method  : 'GET',
+								params  : [
+										'day'       => $request->day,
+										'endtime'   => $request->endtime,
+										'month'     => $request->month,
+										'starttime' => $request->starttime,
+										'timespan'  => $request->timespan,
+										'year'      => $request->year,
+								]
+						);
+
+						$data = json_decode( $response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR );
+				} catch ( Throwable $exception ) {
+						return new Result(
+								success: false,
+								message: $exception->getMessage()
+						);
+				}
+
+				$statistics = collect();
+				foreach ( Arr::get( $data, 'data', [] ) as $stat ) {
+						$statistics->push( new MailcountStatistics(
+								bounces_in      : Arr::get( $stat, 'bounces_in', 0 ),
+								bounces_out     : Arr::get( $stat, 'bounces_out', 0 ),
+								count           : Arr::get( $stat, 'count', 0 ),
+								count_in        : Arr::get( $stat, 'count_in', 0 ),
+								count_out       : Arr::get( $stat, 'count_out', 0 ),
+								index           : Arr::get( $stat, 'index', 0 ),
+								pregreet_rejects: Arr::get( $stat, 'pregreet_rejects', 0 ),
+								rbl_rejects     : Arr::get( $stat, 'rbl_rejects', 0 ),
+								spamcount_in    : Arr::get( $stat, 'spamcount_in', 0 ),
+								spamcount_out   : Arr::get( $stat, 'spamcount_out', 0 ),
+								time            : Arr::get( $stat, 'time', 0 ),
+								viruscount_in   : Arr::get( $stat, 'viruscount_in', 0 ),
+								viruscount_out  : Arr::get( $stat, 'viruscount_out', 0 )
+						) );
+				}
+
+				return new Result(
+						success: true,
+						data   : [
+								'statistics' => $statistics,
+						]
+				);
+		}
+}
