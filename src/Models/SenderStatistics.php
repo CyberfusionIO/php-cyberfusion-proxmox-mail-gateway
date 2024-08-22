d7a54ffcb1323f5648d57c65854973c09ebf26e1
Lars Jansen <lars@ljpc.nl>
2024-08-22 14:16:12 +0200

    feat: /statistics/sender endpoint

---
+++ src/Models/SenderStatistics.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Models;
+
+/**
+ * Model representing sender address statistics.
+ */
+class SenderStatistics {
+		/**
+		 * @param int $bytes Mail traffic (Bytes).
+		 * @param string $sender Sender email.
+		 * @param int|null $count Mail count.
+		 * @param int|null $viruscount Number of sent virus mails.
+		 */
+		public function __construct(
+				public int    $bytes,
+				public string $sender,
+				public ?int   $count = null,
+				public ?int   $viruscount = null
+		) {
+		}
+}
---
+++ src/Requests/SenderRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Requests;
+
+/**
+ * Request class for fetching sender address statistics.
+ */
+class SenderRequest
+{
+		/**
+		 * @param int|null $day Day of month. Get statistics for a single day. (1-31)
+		 * @param int|null $endtime Only consider entries older than 'endtime' (unix epoch). This is set to '<start> + 1day' by default.
+		 * @param string|null $filter Sender address filter.
+		 * @param int|null $month Month. You will get statistics for the whole month if you do not specify a day. (1-12)
+		 * @param string|null $orderby Remote sorting configuration(JSON, ExtJS compatible).
+		 * @param int|null $starttime Only consider entries newer than 'starttime' (unix epoch). Default is 'now - 1day'.
+		 * @param int|null $year Year. Defaults to current year. You will get statistics for the whole year if you do not specify a month or day. (1900-3000)
+		 */
+		public function __construct(
+				public ?int $day = null,
+				public ?int $endtime = null,
+				public ?string $filter = null,
+				public ?int $month = null,
+				public ?string $orderby = null,
+				public ?int $starttime = null,
+				public ?int $year = null
+		) {}
+}
