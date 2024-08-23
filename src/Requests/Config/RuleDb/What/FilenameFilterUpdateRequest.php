2a87bdbf8635604814ee5ffa84e6adef5f1a77b9
Lars Jansen <lars@ljpc.nl>
2024-08-23 11:14:55 +0200

    feat: Add FilenameFilter and VirusFilter endpoints for /config/ruledb/what/{ogroup}/filenamefilter and /config/ruledb/what/{ogroup}/virusfilter

---
+++ src/Requests/Config/RuleDb/What/FilenameFilterUpdateRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Requests\Config\RuleDb\What;
+
+class FilenameFilterUpdateRequest
+{
+    /**
+     * @param int $ogroup Object Groups ID.
+     * @param int $id Object ID.
+     * @param string $filename Filename filter
+     */
+    public function __construct(
+        public int $ogroup,
+        public int $id,
+        public string $filename,
+    ) {
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'filename' => $this->filename,
+        ];
+    }
+}
