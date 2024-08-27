ee9c7288de43faffacbd6c7d8c283afdf57cc5e6
Lars Jansen <lars@ljpc.nl>
2024-08-27 10:07:13 +0200

    feat: add /config/cluster endpoints (create, join, update-fingerprints)

---
+++ src/Requests/Config/Cluster/CreateClusterRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Requests\Config\Cluster;
+
+class CreateClusterRequest
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * Convert the request parameters to an array.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [];
+    }
+}
---
+++ src/Requests/Config/Cluster/UpdateFingerprintsRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cyberfusion\ProxmoxMGW\Requests\Config\Cluster;
+
+class UpdateFingerprintsRequest
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * Convert the request parameters to an array.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [];
+    }
+}
