ca542f663fe1f9d3a965ac833c2f30ded7e0d2d8
Lars Jansen <lars@ljpc.nl>
2024-08-28 09:52:48 +0200

    feat: Add Whitelist ReceiverRegex and IP endpoints

--- src/Endpoints/Config/Whitelist/IpEndpoint.php
+++ src/Endpoints/Config/Whitelist/IpEndpoint.php
@@ -3,70 +3,32 @@
 namespace Cyberfusion\ProxmoxMGW\Endpoints\Config\Whitelist;
 
 use Cyberfusion\ProxmoxMGW\Endpoints\Endpoint;
-use Cyberfusion\ProxmoxMGW\Models\Config\Whitelist\Ip;
-use Cyberfusion\ProxmoxMGW\Requests\Config\Whitelist\IpGetRequest;
-use Cyberfusion\ProxmoxMGW\Requests\Config\Whitelist\IpUpdateRequest;
+use Cyberfusion\ProxmoxMGW\Requests\Config\Whitelist\IpCreateRequest;
 use Cyberfusion\ProxmoxMGW\Support\Result;
-use Illuminate\Support\Arr;
 use Throwable;
 
 class IpEndpoint extends Endpoint
 {
     /**
-     * Read 'IP Address' object settings.
+     * Add 'IP Address' object.
      *
-     * @param IpGetRequest $request
+     * @param IpCreateRequest $request
      * @return Result
      */
-    public function get(IpGetRequest $request): Result
+    public function create(IpCreateRequest $request): Result
     {
         try {
             $response = $this->client->makeRequest(
-                endpoint: sprintf('/config/whitelist/ip/%d', $request->id),
-                method: 'GET',
+                endpoint: '/config/whitelist/ip',
+                method: 'POST',
+                params: $request->toArray(),
             );
 
             $data = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
         } catch (Throwable $exception) {
-            return new Result(
-                success: false,
-                message: $exception->getMessage(),
-            );
+            return new Result(success: false, message: $exception->getMessage());
         }
 
-        return new Result(
-            success: true,
-            data: [
-                'ip' => new Ip(
-                    id: Arr::get($data, 'id'),
-                ),
-            ],
-        );
-    }
-
-    /**
-     * Update 'IP Address' object.
-     *
-     * @param IpUpdateRequest $request
-     * @return Result
-     */
-    public function update(IpUpdateRequest $request): Result
-    {
-        try {
-            $this->client->makeRequest(
-                endpoint: sprintf('/config/whitelist/ip/%d', $request->id),
-                method: 'PUT',
-                params: [
-                    'ip' => $request->ip,
-                ],
-            );
-        } catch (Throwable $exception) {
-            return new Result(
-                success: false,
-                message: $exception->getMessage(),
-            );
-        }
-
-        return new Result(success: true);
+        return new Result(success: true, data: ['id' => $data]);
     }
 }
